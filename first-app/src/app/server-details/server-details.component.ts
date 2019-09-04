c14b7493fc10a308c7d301685ba2bcbf6cadf865
Faddah Wolf <my_biz@me.com>
2019-09-04 12:51:30 -0700

    Created server-details component with angular-cli; put button in html.

---
+++ first-app/src/app/server-details/server-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-details',
+  templateUrl: './server-details.component.html',
+  styleUrls: ['./server-details.component.css']
+})
+export class ServerDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onDisplayDeviceDetails() {
+    console.log('Device Details button clicked!');
+  }
+
+}
